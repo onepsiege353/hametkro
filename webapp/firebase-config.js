ac871aef6ac36c8e25df6618c955b0f6a1cdeb71
onepsiege353 <onepsiege353@users.noreply.github.com>
2026-09-06 02:26:48 +0000

    Brancher la plateforme web sur Firebase réel (Auth + Firestore + Storage) : navigation libre, transactions sécurisées, commission admin; règles mises à jour

---
+++ webapp/firebase-config.js
@@ -0,0 +1,10 @@
+// Configuration Firebase du projet "hametkro".
+// NB: une clé API Web n'est PAS un secret (la sécurité vient des règles).
+window.HAM_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAf0gcAGddEDkOiJMUrjFUrsd-MzvgYLaQ",
+  authDomain: "hametkro.firebaseapp.com",
+  projectId: "hametkro",
+  storageBucket: "hametkro.firebasestorage.app",
+  messagingSenderId: "1071001951580",
+  appId: "1:1071001951580:web:7217e57ea6e9362e41b599"
+};
